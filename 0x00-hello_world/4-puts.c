29da383a4183756107ca5c830253fb4c1555e416
esther <mmesomaesther002@gmail.com>
2022-09-11 17:37:17 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: always 0
- *     */
+ * main - entry point
+ *
+ * Return: alway returns 0
+ */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));                                               printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
