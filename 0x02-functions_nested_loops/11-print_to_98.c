3711dcbdab3b6885e7a87f628e7073cf32c417e9
esther <mmesomaesther002@gmail.com>
2022-09-14 06:54:22 -0700

    it

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 
 /**
- *  *                  in order separated by a comma followed b
- *  y a space.
- *   *@n: The number to begin counting at.
- *    */
+ * print_to_be_98 - prints all natural numbers from input to 98,
+ *                  in order separated by a comma followed by a space
+ * @n: The number to begin counting at.
+ */
 void print_to_be_98(int n)
 {
-	        if (n >= 98)
-			        {
-					                while (n > 98)
-								                        printf("%d, ", n--);
-							                printf("%d\n", n);
-									        }
-		        else
-				        {
-						                while (n < 98)
-									                        printf("%d, ", n++);
-								                printf("%d\n", n);
-										        }
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
 }
-~
-~
-<_to_be_98.c" 22L, 390C written
-4;mroot@771cc6613908:/alx-low_level_prog
