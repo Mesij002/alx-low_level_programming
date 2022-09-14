52201907f2b6f7d79afcb3c3b557c28f73ebb960
esther <mmesomaesther002@gmail.com>
2022-09-14 06:41:53 -0700

    commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *  *                  in order separated by a comma followed b
+ *  y a space.
+ *   *@n: The number to begin counting at.
+ *    */
+void print_to_be_98(int n)
+{
+	        if (n >= 98)
+			        {
+					                while (n > 98)
+								                        printf("%d, ", n--);
+							                printf("%d\n", n);
+									        }
+		        else
+				        {
+						                while (n < 98)
+									                        printf("%d, ", n++);
+								                printf("%d\n", n);
+										        }
+}
+~
+~
+<_to_be_98.c" 22L, 390C written
+4;mroot@771cc6613908:/alx-low_level_prog
