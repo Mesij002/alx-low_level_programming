9e7d49540b3ccdb19919e76fcb8d8f5d2ed97458
esther <mmesomaesther002@gmail.com>
2022-09-14 17:30:53 -0700

    else

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,32 +1,28 @@
+
 #include "main.h"
 /**
- * jack_bauer - func that prints every minute of the day of jack Bauer, starti
- * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
- * and resets mins 
- * Return: 0
- */
+ * * jack_bauer - function
+ * */
 void jack_bauer(void)
 {
-	int hours = 0;
-	int minutes = 0;
-	int hours_remainder;
-	int mins_remainder;
+		int hour, h1, h2, min, m1, m2;
 
-	while (hours <= 23)
-	{
-		while (minutes <= 59)
-		{
-av
-			hours_remaining = hours % 10;
-			_putchar(hours / 10 + '0');
-			_putchar(hours_remainder + '0');
-			_putchar(':');
-			_putchar(minutes / 10 + '0');
-			_putchar(mins_remainder + '0');
-			minutes++;
-			_putchar('\n');
-		}
-		hours++;
-		minutes = 0;
-	}
+			for (hour = 0; hour <= 23; ++hour)
+					{
+								h1 = hour / 10;
+										h2 = hour % 10;
+												for (min = 0; min < 60; ++min)
+															{
+																			m1 = min / 10;
+																						m2 = min % 10;
+																									_putchar(h1 + '0');
+																												_putchar(h2 + '0');
+																															_putchar(':');
+																																		_putchar(m1 + '0');
+																																					_putchar(m2 + '0');
+																																								_putchar(10);
+																																										}
+														min = 0;
+															}
 }
+
