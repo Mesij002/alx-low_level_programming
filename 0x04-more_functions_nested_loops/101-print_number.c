e0a1edfdec91d85e3d1044954960475a02e4b10c
esther <mmesomaesther002@gmail.com>
2022-09-16 09:57:34 -0700

    come

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,17 +3,14 @@
 
 /**
  * print_number - prints a number
- *
  * @n: The number to print
  */
 
 void print_number(int n)
-
 {
 	unsigned int num = n;
 
 	if (n < 0)
-
 	{
 		putchar('-');
 		num = -num;
