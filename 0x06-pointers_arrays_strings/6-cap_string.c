586fa8eee3e4323efa2f7f8550607443339ecf13
esther <mmesomaesther002@gmail.com>
2022-09-23 03:38:01 -0700

    yea

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,49 +1,31 @@
 #include "main.h"
-
 /**
+ * cap_string - capitalizes everey word of a string
  *
- *  cap_string - capitalizes all words of a string
- *  @s: input string.
- *  Return: the pointer to s.
+ * @s: string to modify
+ *
+ * Return: the resulting string
  */
 
 char *cap_string(char *s)
 {
-	int count = 0, i;
-	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int x = 0, y;
+	char special[13] = {9, 10, 32, 33, 34, 40, 41,
+		44, 46, 59, 63, 123, 125};
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-
-		*(s + count) = *(s + count) - 32;
-
-	count++;
-
-	while (*(s + count) != '\0')
-
+	while (*(s + x))
 	{
-
-		for (i = 0; i < 13; i++)
-
+		for (y = 0; y < 13; y++)
 		{
-
-			if (*(s + count) == separators[i])
-
+			if (x == 0 && s[x] >= 97 && s[x] <= 122)
+				s[x] -= 32;
+			if (s[x - 1] == special[y])
 			{
-
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-
-				break;
-
+				if ((*(s + x) >= 97) &&  (*(s + x) <= 122))
+					*(s + x) -= 32;
 			}
-
 		}
-
-		count++;
-
+		x++;
 	}
-
 	return (s);
-
 }
